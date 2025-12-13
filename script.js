44b5790e7ed4d48cccf6c1663ef22f7bc6ab5a96
shauryaupadhyaya <shaurya.upadhyaya@myglobal.school>
2025-12-13 16:58:24 +0800

    backrgound settings page

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 //background
 document.addEventListener("DOMContentLoaded", () => {
     const bg = document.getElementById("background")
-    const bgInput = document.getElementById("bgupload")
+    const bgInput = document.getElementById("bgInput")
+    const preview = document.getElementById("bgPreview")
+
+    const savedBg = localStorage.getItem("userBg")
+    if(savedBg) {
+        bg.style.backgroundImage = savedBg
+        preview.style.backgroundImage = savedBg
+    }
 
     bgInput.addEventListener("change", e => {
         const file = e.target.files[0]
         if (!file) return
-        const url = URL.createObjectURL(file)
-        bg.style.backgroundImage = `url(${url})`
-        bg.style.backgroundColor = "transparent"
-        bg.style.backgroundSize = "cover"
-        bg.style.backgroundPosition = "center"
+
+        const reader = new FileReader()
+        reader.onload = () => {
+            preview.style.backgroundImage = `url(${reader.result})`
+        }
+        reader.readAsDataURL(file)
     })
 
+    document.getElementById("saveBgBtn").addEventListener("click", () => {
+        const style = preview.style.backgroundImage
+        if (!style) return
+
+        localStorage.setItem("userBg", style)
+        bg.style.backgroundImage = style
+    })
 })
 
 //opening and closing windows
@@ -87,5 +102,22 @@ document.querySelectorAll(".window").forEach(win => {
     win.addEventListener("mousedown", () => {
         topZ++;
         win.style.zIndex = topZ
+    })
+})
+
+// tab switching settings
+const tabs = document.querySelectorAll(".settings-tab");
+const pages = {
+    appearance: document.getElementById("appearance-page"),
+    theme: document.getElementById("theme-page")
+}
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(t => t.classList.remove("active"));
+        tab.classList.add("active");
+
+        Object.values(pages).forEach(p => p.classList.add("hidden"));
+        pages[tab.dataset.tab].classList.remove("hidden");
     })
 })
